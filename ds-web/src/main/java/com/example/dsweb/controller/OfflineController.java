e26a537b65d28b6b4951ee7ddcdac30b2e101f79
mashengpeng <1360311705@qq.com>
2021-12-06 14:53:12 +0800

    修改与前端接口一致

--- ds-web/src/main/java/com/example/dsweb/controller/OfflineController.java
+++ ds-web/src/main/java/com/example/dsweb/controller/OfflineController.java
@@ -12,13 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @Slf4j
 public class OfflineController {
@@ -26,8 +25,15 @@ public class OfflineController {
     @Autowired
     OfflineDao offlineDao;
 
+
+    @GetMapping("/offline/size")
+    public BaseResVO getOfflineCount() {
+        int count = offlineDao.getOfflineCount();
+        return ResultVOUtils.success(count);
+    }
+
     /**
-     * offlinetable连表查询
+     * offlinetable查询
      *
      * @param clientTableReqVO
      * @return
--- ds-web/src/main/java/com/example/dsweb/dao/OfflineDao.java
+++ ds-web/src/main/java/com/example/dsweb/dao/OfflineDao.java
@@ -10,6 +10,8 @@ import java.util.List;
 @Mapper
 public interface OfflineDao {
 
+    int getOfflineCount();
+
     List<OfflineMes> findAllOfflineByLimit(Integer currIndex, Integer pageSize);
 
     OfflineMes findOfflineByIp(String ip);
